000b331192e804c632556c5061aa5f40f32d0ffc
rayane <rayanesantos-131@hotmail.com>
2017-07-06 23:34:48 -0300

    config-template-3

---
+++ app/assets/javascripts/site.js
@@ -0,0 +1,10 @@
+
+
+//= jquery.min.js
+//= bootstrap.min.js
+//= countdown.js
+//= slick.min.js
+//=	gmaps.min.js
+//= magnific-popup.min.js
+//= main.js   
+
---
+++ app/controllers/site_controller.rb
@@ -0,0 +1,74 @@
+class SiteController < ApplicationController
+  layout 'site'
+
+  
+  def index
+  end
+
+  def academic
+  end
+
+  def beta
+  end
+
+  def certificates
+  end
+
+  def comitees
+  end
+
+  def contact
+  end
+
+  def cronogram
+  end
+
+  def diretory
+  end
+
+  def faq
+  end
+
+  def fortaleza
+  end
+
+  def inscription
+  end
+
+  def muns
+  end
+
+  def parties
+  end
+
+  def partners
+  end
+
+  def perfil
+  end
+
+  def schools
+  end
+
+  def secretary
+  end
+
+  def apresentation
+  end
+
+  def solidary
+  end
+
+  def speeches
+  end
+
+  def story
+  end
+
+  def story
+  end
+
+  def press
+  end
+
+end
